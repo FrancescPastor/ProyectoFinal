b4f47583bb3c2da78f84f2f3c141eb5dfec730e9
FrancescPastor <xescu00@gmail.com>
2020-05-03 16:18:26 +0200

    va la camara pero le falta hacerlo con los sockets

--- cliente/js/alumno.js
+++ cliente/js/alumno.js
@@ -1,102 +1,72 @@
+
+
 Vue.component('alumno', {
   template:/*html*/ `
 <div>
 
 <button @click="probarconexion">Conexion</button>
-<button @click="conexionCamara">Camara</button>
-<button @click="recuperarToken">Token</button>
+<button id="btn1">dsfsd</button>
+
 
 <video id="localVideo" autoplay playsinline controls="false"/>
+<video id="friendStream" autoplay playsinline controls="false"/>
 <textarea rows="3" width="300px" cols="50" id="txtMySignal"></textarea>
 <input type="text" placeholder="El token de tu amigo" ref="txtTextSignal"></input>
-<button @click="enviarToken">Connectar</button>
+
 </div> 
 
   `,
 
   data() {
-    //varaible para utilizar la peer que luego la retorno como p en return i asi la puedo usar en todas las funciones de methods.
-    var p1 = new SimplePeer ({initiator: location.hash === '#/', trickle: false});
+   
+
     return {
-      vacia: [],
-      p : p1
-      
+      vacia: []
+
+
     }
   },
   methods: {
+
     
-    //esto se va a borrar en un futuro 
     probarconexion: function (event) {
       let socket = io.connect('http://localhost:8888');
       socket.on('hola', function (data) {
         console.log(data);
       })
     },
-
-    conexionCamara: function (event) {
-      const constraints = {
-        'video': true,
-        'audio': true
+    playVideo: function (stream, idVideo) {
+      const video = document.getElementById(idVideo);
+      video.srcObject = stream;
+      video.onloadedmetadata = function () {
+        video.play();
       }
-      /**Promesa que mira si estan conectados los dispositivos y si el usuario 
-      *le da los permisos entonces si la promesa sale bien llamo a la funciun que muestra
-      *la camara por pantalla y si no es que no hay dispositivos o no tiene permisos
-      *entonces printa un console log 
-      */
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then(stream => {
-          //Miramos las camaras que hay conectadas y micros
-          function getConnectedDevices(type, callback) {
-            navigator.mediaDevices.enumerateDevices()
-              .then(devices => {
-                const filtered = devices.filter(device => device.kind === type);
-                callback(filtered);
 
-              });
-          }
-          getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras));
-          //Llamar funcion que activa la camara y la muestra por pantalla.  
-          playVideoFromCamera();
-          //funcion que muestra la camara por pantalla
-          async function playVideoFromCamera() {
-
-            try {
-              const constraints = { 'video': true, 'audio': true };
-              const stream = await navigator.mediaDevices.getUserMedia(constraints);
-              const videoElement = document.querySelector('video#localVideo');
-              videoElement.srcObject = stream;
-            } catch (error) {
-              console.error('Error opening video camera.', error);
-            }
-          }
-          //En este console log tenemos la variable stream que te da la id del stream      
-          console.log('Got MediaStream:', stream);
-        })
-        .catch(error => {
-          console.error('Error accessing media devices.', error);
-        });
     },
-    recuperarToken : function (event){
-     
-      this.p.on('signal', token=>{
-        txtMySignal.innerHTML = JSON.stringify(token);
-      });
+    openStream: function () {
+      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(stream =>{ 
+          this.playVideo(stream, 'localVideo')
+          const p = new SimplePeer ({initiator: location.hash === "#/", trickle: false, stream});
+          p.on('signal', token =>{
+            txtMySignal.innerHTML = JSON.stringify(token);
+          });
+        
+       $('#btn1').click(()=>{
+        let friendSignal = JSON.parse(this.$refs.txtTextSignal.value);
+        p.signal(friendSignal);
+       })
+       // let friendSignal = JSON.parse(this.$refs.txtTextSignal.value);
+          p.on('stream', friendStream => this.playVideo(friendStream, 'friendStream'))
+          })
+        .catch(err => console.log(err));
     },
-    enviarToken : function (event){
     
-      //con el this.refs cojo el valor del value del input text area que tenemos en el template
-     let friendSignal = JSON.parse(this.$refs.txtTextSignal.value);
-     this.p.signal(friendSignal);
-     this.recuperarToken();
-     this.verData();
-    },
-    verData : function (event){
-      this.p.on('connect', ()=>{
-        setInterval(()=> this.p.send(Math.random()), 2000);
-      });
-      this.p.on('data', data => console.log(data));
-    }
 
+  },
+  created: function () {
+    this.openStream();
+ 
   }
 
 });
@@ -141,9 +111,9 @@ const contacte = {
 
 const rutes = {
   '#/': principal,
-  '#1/':segundo,
+  '#1/': segundo,
   '#/contacte': contacte,
-  
+
 
 };
 
